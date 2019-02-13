ffee3c0755540ddadd411affeaf688320f2cbfa1
thpar <git@thpar.gent>
2019-02-13 16:45:35 +0100

    Add experiments table to home page

--- public/app/components/home/homeController.js
+++ public/app/components/home/homeController.js
@@ -1,4 +1,6 @@
 angular.module("CAMEL")
-.controller('HomeController', function() {
+.controller('HomeController', function($location, $routeParams, $route, Experiment) {
     var ctrl = this;
+
+    ctrl.experiments = Experiment.query();
 });
