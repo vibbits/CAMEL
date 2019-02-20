b8876df50691e28ecac509b453fb553d23f3951e
thpar <git@thpar.gent>
2019-02-20 18:04:59 +0100

    Setup for showing only selected fields

--- public/app/components/experiments/experimentsController.js
+++ public/app/components/experiments/experimentsController.js
@@ -1,10 +1,18 @@
 angular.module("CAMEL")
     .controller('ExperimentsController', function($scope, $location, $timeout, $routeParams, $route, $http, Experiment, Field) {
 	var ctrl = this;
-
-	ctrl.fields = Field.query();	
+	var showNr = 5;
+	
+	ctrl.fields = Field.query(function(){
+	    for (i in ctrl.fields){
+		if (i<showNr){
+		    ctrl.fields[i].show = true;
+		} else {
+		    ctrl.fields[i].show = false;
+		}
+	    }
+	});
+	
 	ctrl.experiments = Experiment.query();
-	
-	
 	
     });
