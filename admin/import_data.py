874ca57c43446c25dcee5360085809a73133b63e
Thomas Van Parys <thomas.vanparys@vib.be>
2019-04-09 15:23:37 +0200

    Don't use 'and' to split up TEXT

--- admin/import_data.py
+++ admin/import_data.py
@@ -88,7 +88,11 @@ def parse(input_file_name, field_map, species_map, db):
                         field_type = field_map['fields'][colName]['type']
                         field_unit = field_map['fields'][colName]['unit']
 
-                        multiple_values = [v.strip() for v in re.split('and|AND', raw_field_value)]
+                        if field_type != "value_TEXT":
+                            multiple_values = [v.strip() for v in re.split(', |and|AND', raw_field_value)]
+                        else:
+                            multiple_values = [raw_field_value]
+                            
                         for field_value in multiple_values:
                             if field_unit:
                                 ## Remove units when repeated inside the field
