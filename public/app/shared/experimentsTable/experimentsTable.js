0fe6844c1f5d0c22025ed47283bf8ea93e3814e7
Thomas Van Parys <thomas.vanparys@vib.be>
2019-04-19 11:36:29 +0200

    Complete experiment comparator. Sort on any visible field in the table.

--- public/app/shared/experimentsTable/experimentsTable.js
+++ public/app/shared/experimentsTable/experimentsTable.js
@@ -1,3 +1,6 @@
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
 angular.module("CAMEL")
 .directive('experimentsTable', function($location) {
     return {
@@ -12,8 +15,10 @@ angular.module("CAMEL")
 		$location.path("/experiment/"+experiment.id);
 	    };
 	    scope.orderKey = '';
+	    scope.keyRealm = 'exp';
 	    scope.orderDesc = false;
-	    scope.sortExperiments = function(key){
+	    scope.sortExperiments = function(key, keyRealm='exp'){
+		scope.keyRealm=keyRealm;
 		if (scope.orderKey != key){
 		    scope.orderKey = key;
 		    scope.orderDesc = false;
@@ -26,9 +31,52 @@ angular.module("CAMEL")
 		    }		    
 		}
 	    }
+	    var count=0;
+	    //orderBy experiments
 	    scope.fieldComparator = function(exp1, exp2){
-		if (scope.orderKey == 'name'){
-		    return (exp1['name'] < exp2['name'])? -1:-1;
+		//both experiments should be objects. if not, keep their natural order.
+		if (!scope.orderKey || exp1.type !== 'object' || exp2.type !== 'object'){
+		    return (exp1.index < exp2.index) ? -1 : 1;
+		}
+
+		//order by direct experiment fields
+		if (scope.keyRealm=='exp'){
+		    return (exp1.value[scope.orderKey].toLowerCase() < exp2.value[scope.orderKey].toLowerCase())? -1:1;
+		}
+
+		//order by reference fields
+		if (scope.keyRealm=='ref'){
+		    //TODO
+		}
+
+		if (scope.keyRealm=='fields'){
+		    //order by dynamic fields
+		    
+		    //non-existent fields are at the end of the list
+		    //existing fields are arrays: order by the first item
+		    if (exp1.value.fields.hasOwnProperty(scope.orderKey)){
+			field1 = exp1.value.fields[scope.orderKey][0];
+			if (isNumeric(field1)){
+			    field1 = parseFloat(field1);
+			} else {
+			    field1 = field1.toLowerCase();
+			}
+		    } else {
+			return 1;
+		    }
+		    if (exp2.value.fields.hasOwnProperty(scope.orderKey)){
+			field2 = exp2.value.fields[scope.orderKey][0];
+			if (isNumeric(field1)){
+			    field2 = parseFloat(field2);
+			} else {
+			    field2 = field2.toLowerCase();
+			}
+		    } else {
+			return -1;
+		    }
+
+		    //compare given fields
+		    return (field1 < field2)? -1:1;
 		}
 	    }
         }
