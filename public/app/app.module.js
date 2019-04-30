409c075884e815a8e85033e40e48799e7be616df
Thomas Van Parys <thomas.vanparys@vib.be>
2019-04-30 15:21:04 +0200

    Remove unused navigation functions

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -1,21 +1,4 @@
 var app = angular.module('CAMEL', ['ngResource','ngRoute']);
 app.controller('AppController', function($scope, $location, $route, $document) {
-
-    $scope.navigateTo = function(path, savePath) {
-        if (savePath) {
-            $location.search("returnUrl", $location.path());
-        }
-        $route.reload();
-        $location.path("/"+path);
-        if ($(".navbar-header").css("float") == 'none') {
-            $("#contra-navbar-collapse").collapse('toggle');
-        }
-    }
-
-    $scope.isCurrent = function(path, level) {
-        if (!level) level = 1;
-        var loc = $location.path().split("/");
-        return loc[level] == path;
-    }
-
+    
 });
