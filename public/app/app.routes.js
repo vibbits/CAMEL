e7324b3575644b1b2791533bbb53150e6c735574
Thomas Van Parys <thomas.vanparys@vib.be>
2019-05-20 18:39:20 +0200

    Clean up routes and catch Non Auth event

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -1,39 +1,42 @@
 angular.module('CAMEL')
-.config(['$routeProvider', function($routeProvider) {
+    .config(['$routeProvider', function($routeProvider) {
 	$routeProvider
-	.when('/home', {
+	    .when('/home', {
 		templateUrl:  'app/components/home/homeTemplate.html',
 		controller: 'HomeController',
 		controllerAs: 'home'
-	})
-    .when('/experiment/:id', {
+	    })
+	    .when('/experiment/:id', {
 		templateUrl:  'app/components/experiment/experimentTemplate.html',
 		controller: 'ExperimentController',
 		controllerAs: 'experiment'
-	})
-    .when('/experiments', {
+	    })
+	    .when('/experiments', {
 		templateUrl:  'app/components/experiments/experimentsTemplate.html',
 		controller: 'ExperimentsController',
 		controllerAs: 'experiments'
-	})
-    .when('/fields', {
+	    })
+	    .when('/fields', {
 		templateUrl:  'app/components/fields/fieldsTemplate.html',
 		controller: 'FieldsController',
 		controllerAs: 'fields',
-        data: {'protected': true}
-	})
-    .otherwise({
+		data: {'protected': true}
+	    })
+	    .otherwise({
 		redirectTo: '/home'
-	});
-}])
-.run(function($rootScope, AUTH_EVENTS, AuthService){
-    $rootScope.$on('$routeChangeStart', function(event, next){
-        if (next.$$route.hasOwnProperty('data') && next.$$route.data.hasOwnProperty('protected')){
-            var protected = next.$$route.data.protected;
-            if (protected && !AuthService.isAuthenticated()){
-                event.preventDefault();
-                $rootScope.$broadcast(AUTH_EVENTS.notAuthenticated);
+	    });
+    }])
+    .run(function($rootScope, AUTH_EVENTS, AuthService){
+	$rootScope.$on('$routeChangeStart', function(event, next){
+            if (next.hasOwnProperty('$$route') && next.$$route.hasOwnProperty('data') && next.$$route.data.hasOwnProperty('protected')){
+		var protected = next.$$route.data.protected;
+		if (protected && !AuthService.isAuthenticated()){
+                    event.preventDefault();
+                    $rootScope.$broadcast(AUTH_EVENTS.notAuthenticated);
+		}
             }
-        }
+	});	
+	$rootScope.$on(AUTH_EVENTS.notAuthenticated, function(event){
+	    console.log("Not Authenticated Event");
+	});
     });
-});
