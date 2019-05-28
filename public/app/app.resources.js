3d88778967d1f11b5b2493f4d3753e2d80589159
Thomas Van Parys <thomas.vanparys@vib.be>
2019-05-28 12:00:41 +0200

    use lowercase 'id' parameters in resources to match data

--- public/app/app.resources.js
+++ public/app/app.resources.js
@@ -1,7 +1,7 @@
 angular.module("CAMEL")
 .factory("Experiment", function ExperimentFactory($resource) {
-    return $resource("api/experiment/:id", {id: '@ID'});
+    return $resource("api/experiment/:id", {id: '@id'});
 })
 .factory("Field", function FieldFactory($resource) {
-    return $resource("api/field/:id", {id: '@ID'});
+    return $resource("api/field/:id", {id: '@id'});
 });
