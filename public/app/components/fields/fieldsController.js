0aa427068e14bb7edc65ff0a116ed9566283d28d
Thomas Van Parys <thomas.vanparys@vib.be>
2019-05-28 10:44:46 +0200

    Simple field add/remove interface

--- public/app/components/fields/fieldsController.js
+++ public/app/components/fields/fieldsController.js
@@ -6,5 +6,33 @@ angular.module("CAMEL")
 	ctrl.fields = Field.query(function(){
 	    ctrl.loaded = true;
 	});
-	
+
+	ctrl.newRow = function(){
+	    var newField = {
+		title: '',
+		unit: '',
+		description: '',
+		type_column: 'value_VARCHAR',
+		link: 0,
+		required: 0,
+		group: 0,
+		group_id: null
+	    };
+	    ctrl.fields.push(newField);
+	};
+
+	ctrl.removeRow = function(field){
+	    if (!field.hasOwnProperty('id')){
+		//new fields can simply be removed
+		var index = ctrl.fields.indexOf(field);
+		if (index !== -1){
+		    ctrl.fields.splice(index, 1);
+		}
+	    } else {
+		//deleting an existing field causes all data
+		//to be removed!
+		alert("Are you sure?");
+		
+	    }
+	}
     });
