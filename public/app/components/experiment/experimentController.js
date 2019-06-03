c434783a25dfa91bd114dff60446cde2420aa596
Thomas Van Parys <thomas.vanparys@vib.be>
2019-06-03 16:31:51 +0200

    Edit button on experiment page

--- public/app/components/experiment/experimentController.js
+++ public/app/components/experiment/experimentController.js
@@ -1,6 +1,12 @@
 angular.module("CAMEL")
     .controller('ExperimentController', function($scope, $location, $timeout, $routeParams, $route, $http, Experiment, Field) {
-		
+	var ctrl = this;
+	
 	$scope.exp = Experiment.get($routeParams);
 	$scope.fields = Field.query();
+
+	ctrl.edit = function(){
+	    $location.path('/experiment/edit/'+$routeParams['id']);
+	}
+	
     });
