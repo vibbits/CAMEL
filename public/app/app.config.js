b9fd92bc4057bf07027ceba1b97f425d86c7b2ae
James Collier <james.collier@vib.be>
2021-02-24 14:24:35 +0100

    defaul config files

---
+++ public/app/app.config.js
@@ -0,0 +1,7 @@
+// Copy this file to `app.config.js` and replace the
+// URLs with the correct hosting locations.
+angular.module("camel.constants", [])
+    .constant('config', {
+	apiUrl: "http://localhost:8888/api",
+	attachments: "http://localhost:8888/camel_files"
+    });
